b6507bc7d7be53a213dc55d5d52c2e3d9ded157d
scolpig <scolpig@gmail.com>
2021-06-29 12:16:57 +0900

    faces detection

--- exam16_Beauty_GAN.py
+++ exam16_Beauty_GAN.py
@@ -13,9 +13,38 @@ plt.figure(figsize=(16,10))
 plt.imshow(img)
 plt.show()
 
+img_result = img.copy()
+dets = detector(img)
+if len(dets) == 0:
+    print('cannot find faces!')
+else:
+    fig, ax = plt.subplots(1,figsize=(16,10))
+    for det in dets:
+        x, y, w, h = det.left(), det.top(), det.width(), det.height()
+        rect = patches.Rectangle((x,y),w,h,linewidth=2, edgecolor='r',
+                                 facecolor='none')
+        ax.add_patch(rect)
+    ax.imshow(img_result)
+    plt.show()
 
+fig, ax = plt.subplots(1, figsize=(16,10))
+objs = dlib.full_object_detections()
+for detection in dets:
+    s = sp(img, detection)
+    objs.append(s)
+    for point in s.parts():
+        circle = patches.Circle((point.x, point.y), radius=3,
+                                edgecolor='r', facecolor='r')
+        ax.add_patch(circle)
+ax.imshow(img_result)
+plt.show()
 
-
+faces = dlib.get_face_chips(img, objs, size=256, padding=0.3)
+fig, axes = plt.subplots(1, len(faces)+1, figsize=(20,16))
+axes[0].imshow(img)
+for i, face in enumerate(faces):
+    axes[i+1].imshow(face)
+plt.show()
 
 
 
