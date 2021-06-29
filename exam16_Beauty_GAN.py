d42d8a225882eb1faf86cfdaadb21aa5f705e64e
scolpig <scolpig@gmail.com>
2021-06-29 11:33:52 +0900

    중간 점검

---
+++ exam16_Beauty_GAN.py
@@ -0,0 +1,29 @@
+import dlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import numpy as np
+
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor('./models/shape_predictor_5_face_landmarks.dat')
+
+img = dlib.load_rgb_image('./imgs/12.jpg')
+plt.figure(figsize=(16,10))
+plt.imshow(img)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
