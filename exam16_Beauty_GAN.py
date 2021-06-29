75a20943026438a0e4afc2e443501aa4e78cafbc
BBangNa <befritaco@gmail.com>
2021-06-29 16:59:37 +0900

    완료_

--- exam16_Beauty_GAN.py
+++ exam16_Beauty_GAN.py
@@ -7,30 +7,48 @@ import numpy as np
 
 def align_faces(img, detector, sp):
 
-img_result = img.copy()
-dets = detector(img)
-if len(dets) == 0:
-    print('cannot find faces!')
-else:
-    fig, ax = plt.subplots(1,figsize=(16,10))
-    for det in dets:
-        x, y, w, h = det.left(), det.top(), det.width(), det.height()
-        rect = patches.Rectangle((x,y),w,h,linewidth=2, edgecolor='r',
-                                 facecolor='none')
-        ax.add_patch(rect)
-    ax.imshow(img_result)
-    plt.show()
+    dets = detector(img, 1)
+    objs = dlib.full_object_detections()
+    for detection in dets:
+        s = sp(img, detection)
+        objs.append(s)
+    faces = dlib.get_face_chips(img, objs, size=256, padding=0.35)
+    return faces
 
-fig, ax = plt.subplots(1, figsize=(16,10))
-objs = dlib.full_object_detections()
-for detection in dets:
-    s = sp(img, detection)
-    objs.append(s)
-    for point in s.parts():
-        circle = patches.Circle((point.x, point.y), radius=3,
-                                edgecolor='r', facecolor='r')
-        ax.add_patch(circle)
-ax.imshow(img_result)
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor('./models/shape_predictor_68_face_landmarks.dat')
+
+# test_img = dlib.load_rgb_image('./imgs/17.jpg')
+# test_faces = align_faces(test_img, detector, sp)
+# fig, axes = plt.subplots(1, len(test_faces)+1, figsize=(20,16))
+# axes[0].imshow(test_img)
+# for i, face in enumerate(test_faces):
+#     axes[i+1].imshow(face)
+# plt.show()
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer()) # 초기화?
+saver = tf.train.import_meta_graph('./models/model.meta')
+saver.restore(sess, tf.train.latest_checkpoint('./models'))
+graph = tf.get_default_graph()
+X = graph.get_tensor_by_name('X:0')
+Y = graph.get_tensor_by_name('Y:0')
+Xs = graph.get_tensor_by_name('generator/xs:0')
+
+def preprocess(img):
+    return (img / 255. - 0.5) * 2
+def deprocess(img):
+    return (img + 1) / 2
+
+img1 = dlib.load_rgb_image('./imgs/12.jpg')
+img1_faces = align_faces(img1, detector, sp)
+
+img2 = dlib.load_rgb_image('./imgs/makeup/XMY-266.png')
+img2_faces = align_faces(img2, detector, sp)
+
+fig, axes = plt.subplots(1,2,figsize=(16,10))
+axes[0].imshow(img1_faces[0])
+axes[1].imshow(img2_faces[0])
 plt.show()
 
 # 화장을 입히기
