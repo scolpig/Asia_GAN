28ffb6617bbc0a9780094682c20ce6c2c179681a
scolpig <scolpig@gmail.com>
2021-07-01 16:14:01 +0900

    2020년 영화 리뷰 데이터 준비 완료

--- prj02_Movie_for_you/preprocess_one_sentence.py
+++ prj02_Movie_for_you/preprocess_one_sentence.py
@@ -1,22 +1,22 @@
 import pandas as pd
 
-df = pd.read_csv('./crawling/cleaned_review_2020.csv',
+df = pd.read_csv('./crawling/movie_review_2017_2021.csv',
                  index_col=0)
 df.dropna(inplace=True)
-df.to_csv('./crawling/cleaned_review_2020.csv')
+df.to_csv('./crawling/movie_review_2017_2021.csv')
 one_sentences = []
 for idx, title in enumerate(df['titles'].unique()):
-    print(idx)
-    print(title)
-    temp = df[df['titles']==title]['cleaned_sentences']
+    #print(idx)
+    #print(title)
+    temp = df[df['titles']==title]['cleaned_reviews']
     one_sentence = ' '.join(temp)
     one_sentences.append(one_sentence)
 df_one_sentences = pd.DataFrame(
     {'titles':df['titles'].unique(),
      'reviews':one_sentences})
 print(df_one_sentences.head())
-df_one_sentences.to_csv('./crawling/one_sentence_review_2020.csv')
-
+df_one_sentences.to_csv('./crawling/one_sentence_review_2017_2021.csv')
+print(df_one_sentences.info())
 
 
 
